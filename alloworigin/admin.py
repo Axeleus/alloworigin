0257ac70148e8125da6847e59c17cb2e384cc160
Terry <limtaesu@gmail.com>
2015-08-19 10:58:10 -0400

    .

--- alloworigin/admin.py
+++ alloworigin/admin.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 from .models import Request
 
 class RequestAdmin(admin.ModelAdmin):
-    list_display = ('dest','ip')
+    list_display = ('dest','ip','date')
 admin.site.register(Request, RequestAdmin)
